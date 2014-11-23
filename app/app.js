b048d4abc265b91d7960328f09cedba4eb680527
Boris Yankov <borisyankov@gmail.com>
2014-11-23 06:05:04 +0200

    Simple signin and signup forms and validations

--- app/app.js
+++ app/app.js
@@ -1,9 +1,70 @@
 'use strict';
 
-angular.module('leanMetrix', [
-  ])
+angular.module('leanMetrix', [])
 
-  .run(function() {
+.run(function() {
     console.log('run'); //debug
     //simpleLogin.getUser();
+});
+
+angular.module('leanMetrix').controller('signinCtrl', function($scope) {
+
+    var model = this;
+
+    model.message = "";
+
+    model.user = {
+        email: "",
+        password: ""
+    };
+
+    model.submit = function(isValid) {
+        if (isValid) {
+            model.message = "Submitted " + model.user.username;
+        } else {
+            model.message = "There are still invalid fields below";
+        }
+    };
+});
+
+angular.module('leanMetrix').controller('signupCtrl', function($scope) {
+
+    var model = this;
+
+    model.message = "";
+
+    model.user = {
+        email: "",
+        password: "",
+        confirmPassword: ""
+    };
+
+    model.submit = function(isValid) {
+        console.log("hello");
+        if (isValid) {
+            model.message = "Submitted " + model.user.username;
+        } else {
+            model.message = "There are still invalid fields below";
+        }
+    };
+});
+
+
+angular.module('leanMetrix').directive("compareTo", function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
 });
