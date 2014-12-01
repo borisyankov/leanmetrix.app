e53262bfb6036d38b3770dc761dcdd9f53bedcd6
Boris Yankov <borisyankov@gmail.com>
2014-12-01 15:53:22 +0200

    Add 'create account', 'sign in' and list messages controllers

--- app/app.js
+++ app/app.js
@@ -1,13 +1,26 @@
 'use strict';
 
-angular.module('leanMetrix', [])
+var firebase = new Firebase("https://leanmetrix.firebaseio.com");
+
+angular.module('leanMetrix', ['firebase'])
 
 .run(function() {
-    console.log('run'); //debug
-    //simpleLogin.getUser();
+
 });
 
-angular.module('leanMetrix').controller('signinCtrl', function($scope) {
+angular.module('leanMetrix').controller('authCtrl', function($rootScope) {
+    var model = this;
+    firebase.onAuth(function(authData) {
+        model.data = authData;
+        $rootScope.authData = authData;
+    });
+
+    this.signOut = function() {
+        firebase.unauth();
+    }
+});
+
+angular.module('leanMetrix').controller('signInCtrl', function(AccountService) {
 
     var model = this;
 
@@ -20,34 +33,63 @@ angular.module('leanMetrix').controller('signinCtrl', function($scope) {
 
     this.submit = function(isValid) {
         if (isValid) {
-            model.message = "Submitted " + model.user.username;
+            AccountService.signIn(model.user.email, model.user.password).success(function(data) {
+                AuthenticationService.isLogged = true;
+                $window.sessionStorage.token = data.token;
+                $location.path("/admin");
+            }).error(function(status, data) {
+                console.log(status, data);
+            });
         } else {
             model.message = "There are still invalid fields below";
         }
     };
+
+    this.logout = function logout() {
+        if (AuthenticationService.isLogged) {
+            AuthenticationService.isLogged = false;
+            delete $window.sessionStorage.token;
+            $location.path("/");
+        }
+    }
+
 });
 
-angular.module('leanMetrix').controller('createAccountCtrl', function($scope) {
+angular.module('leanMetrix').controller('createAccountCtrl', function(AccountService) {
 
     var model = this;
 
     this.message = "";
 
     this.user = {
-        email: "",
-        password: "",
-        confirmPassword: ""
+        email: "bobtester@mailinator.com",
+        password: "123123",
+        confirmPassword: "123123"
     };
 
     this.submit = function(isValid) {
-        console.log("hello");
         if (isValid) {
-            model.message = "Submitted " + model.user.username;
+            AccountService.create(model.user.email, model.user.password).success(function(data) {
+                AuthenticationService.isLogged = true;
+                $window.sessionStorage.token = data.token;
+                $location.path("/admin");
+            }).error(function(status, data) {
+                console.log(status, data);
+            });
         } else {
             model.message = "There are still invalid fields below";
         }
     };
 });
+
+
+angular.module('leanMetrix').controller('messagesCtrl', function($firebase) {
+    var firebase = new Firebase("https://leanmetrix.firebaseio.com/messages/");
+    var sync = $firebase(firebase);
+
+    this.data = sync.$asArray();
+});
+
 
 
 angular.module('leanMetrix').directive("compareTo", function() {
@@ -69,13 +111,47 @@ angular.module('leanMetrix').directive("compareTo", function() {
     };
 });
 
-angular.module('leanMetrix').directive('usernameAvailableValidator', function($http) {
+angular.module('leanMetrix').directive('usernameAvailableValidator', function($q, AccountService) {
     return {
         require: 'ngModel',
         link: function($scope, element, attrs, ngModel) {
-            ngModel.$asyncValidators.usernameAvailable = function(username) {
-                return $http.get('/api/username-exists?u=' + username);
+            ngModel.$asyncValidators.usernameAvailable = function(email) {
+                var deferred = $q.defer();
+                deferred.resolve(false);
+                return deferred.promise;
+                return AccountService.exist(email);
             };
         }
     }
-})
+});
+
+angular.module('leanMetrix').factory('AccountService', function($firebase, $http) {
+
+    //var firebase = new Firebase("https://leanmetrix.firebaseio.com/");
+
+    return {
+        signIn: function(email, password) {
+            firebase.authWithPassword({
+                email: email,
+                password: password,
+            }, function(error, authData) {
+                if (error === null) {
+                    console.log(authData);
+                } else {
+                    console.log("Error authenticating user:", error);
+                }
+            });
+        },
+        create: function(email, password) {
+            firebase.createUser({
+                email: email,
+                password: password,
+            }, function(error) {
+                console.log(error);
+            });
+        },
+        exist: function(email) {
+            return $http.get('http://localhost:3000/users/?email=' + email);
+        }
+    }
+});
