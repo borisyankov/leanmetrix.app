6b753a5c21d8f444abb816ee42d626ead6f59d59
Boris Yankov <borisyankov@gmail.com>
2014-12-05 22:53:26 +0200

    Reorganize files and build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+//var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var templateCache = require('gulp-angular-templatecache');
 var del = require('del');
 
 var paths = {
+    dest: '../leanmetrix.com/',
+    html: 'index.html',
     lib: [
         'lib/angular/angular.js',
         'lib/angular/angular-messages.js',
@@ -16,7 +19,7 @@ var paths = {
         'app/app.js',
         'app/**/*.js'
     ],
-    style: './public/style.scss',
+    style: 'style/*.css',
     templates: [
         '!app/index.html',
         'app/**/*.html'
@@ -27,33 +30,47 @@ gulp.task('clean', function(cb) {
     del(['build'], cb);
 });
 
+gulp.task('html', ['clean'], function() {
+    return gulp.src(paths.html)
+        .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('style', ['clean'], function() {
+    return gulp.src(paths.style)
+        //.pipe(sourcemaps.init())
+        //    .pipe(sass())
+        //.pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.dest));
+});
+
 gulp.task('lib', ['clean'], function() {
     return gulp.src(paths.lib)
         .pipe(sourcemaps.init())
-            .pipe(uglify())
-            .pipe(concat('lib.min.js'))
+            //.pipe(uglify())
+            .pipe(concat('lib.js'))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(paths.dest));
 });
 
 gulp.task('app', ['clean'], function() {
     return gulp.src(paths.app)
         .pipe(sourcemaps.init())
-            .pipe(uglify({ mangle: false }))
-            .pipe(concat('app.min.js'))
+            //.pipe(uglify({ mangle: false }))
+            .pipe(concat('app.js'))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(paths.dest));
 });
 
 gulp.task('templates', function () {
-    gulp.src(paths.templates)
+    return gulp.src(paths.templates)
         .pipe(templateCache({ module: 'leanMetrix' }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(paths.dest));
 });
 
 gulp.task('watch', function() {
+    gulp.watch(paths.style, ['style']);
     gulp.watch(paths.lib, ['lib']);
     gulp.watch(paths.app, ['app']);
 });
 
-gulp.task('default', ['lib', 'app', 'templates']);
+gulp.task('default', ['html', 'style', 'lib', 'app', 'templates']);
